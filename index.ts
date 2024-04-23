836006569b843bb7d576588e111e96b02c411f78
ManzoorHassan <161144604+ManzoorHassan@users.noreply.github.com>
2024-04-22 22:21:05 -0700

    adding a todos list project

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer'
+let todos = [];
+let condition =true;
+
+while(condition)
+{
+    let addTask = await inquirer.prompt(
+    [
+        {
+            name: "todo",
+            type: "input",
+            message: "what would you like to add in todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "would you like to add more in your todos",
+            default: "false",
+        }
+    ]
+    );
+    todos.push(addTask.todo);
+    condition = addTask.addMore
+    console.log(todos)
+} 
+    
